ec58a9f96dc68be1ef5dd3988bb5e643a9718813
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-01-20 23:54:00 +0600

    small change

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,14 +1,16 @@
 import auth from "@/middlewares/auth";
-import validateRequest from "@/middlewares/validateRequest";
 import { UserController } from "@/user/user.controller";
-import { UserValidation } from "@/user/user.schemas";
 import express from "express";
 import { ENUM_USER_ROLE } from "../../enums/user";
 
 const router = express.Router();
 
 // Route to get all users
-router.get("/all",auth(ENUM_USER_ROLE.SELLER,ENUM_USER_ROLE.USER), UserController.getAllUser);
+router.get(
+  "/all",
+  auth(ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.USER),
+  UserController.getAllUser
+);
 
 // Route to get the profile of the authenticated user
 // Requires the user to have the SELLER role
@@ -17,8 +19,6 @@ router.get(
   auth(ENUM_USER_ROLE.SELLER),
   UserController.getMyProfile
 );
-
-
 
 // Route to get a user by ID
 router.get("/:id", UserController.getOneUser);
